8eae9e3cffeea33c872a0de73a9d442d83191f03
Santhush Ekanayake <109194698+SanthushEk@users.noreply.github.com>
2024-05-11 23:46:04 +0530

    Create salon.sql

---
+++ salon.sql
@@ -0,0 +1,282 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+-- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE salon;
+--
+-- Name: salon; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE salon WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE salon OWNER TO freecodecamp;
+
+\connect salon
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: appointments; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.appointments (
+    appointment_id integer NOT NULL,
+    customer_id integer NOT NULL,
+    service_id integer NOT NULL,
+    "time" character varying(50)
+);
+
+
+ALTER TABLE public.appointments OWNER TO freecodecamp;
+
+--
+-- Name: appointments_appointment_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.appointments_appointment_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.appointments_appointment_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: appointments_appointment_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.appointments_appointment_id_seq OWNED BY public.appointments.appointment_id;
+
+
+--
+-- Name: customers; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.customers (
+    customer_id integer NOT NULL,
+    phone character varying(20),
+    name character varying(100) NOT NULL
+);
+
+
+ALTER TABLE public.customers OWNER TO freecodecamp;
+
+--
+-- Name: customers_customer_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.customers_customer_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.customers_customer_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: customers_customer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.customers_customer_id_seq OWNED BY public.customers.customer_id;
+
+
+--
+-- Name: services; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.services (
+    service_id integer NOT NULL,
+    name character varying(100) NOT NULL,
+    description character varying(100)
+);
+
+
+ALTER TABLE public.services OWNER TO freecodecamp;
+
+--
+-- Name: services_service_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.services_service_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.services_service_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: services_service_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.services_service_id_seq OWNED BY public.services.service_id;
+
+
+--
+-- Name: appointments appointment_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.appointments ALTER COLUMN appointment_id SET DEFAULT nextval('public.appointments_appointment_id_seq'::regclass);
+
+
+--
+-- Name: customers customer_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.customers ALTER COLUMN customer_id SET DEFAULT nextval('public.customers_customer_id_seq'::regclass);
+
+
+--
+-- Name: services service_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.services ALTER COLUMN service_id SET DEFAULT nextval('public.services_service_id_seq'::regclass);
+
+
+--
+-- Data for Name: appointments; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.appointments VALUES (3, 20, 2, '11');
+INSERT INTO public.appointments VALUES (6, 1, 1, '');
+INSERT INTO public.appointments VALUES (13, 1, 1, '');
+INSERT INTO public.appointments VALUES (24, 36, 1, '10');
+INSERT INTO public.appointments VALUES (25, 36, 2, '11');
+
+
+--
+-- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.customers VALUES (1, '', 'santhush');
+INSERT INTO public.customers VALUES (3, '222-2222', 'kevin');
+INSERT INTO public.customers VALUES (4, '111-1111', 'Sam');
+INSERT INTO public.customers VALUES (5, 'Danny', '111-1111');
+INSERT INTO public.customers VALUES (6, '222-2232', 'Nike');
+INSERT INTO public.customers VALUES (17, '444-4567', 'Jim');
+INSERT INTO public.customers VALUES (18, '444-6789', 'Nile');
+INSERT INTO public.customers VALUES (20, '888-8888', 'sadun');
+INSERT INTO public.customers VALUES (36, '555-5555', 'kavindu');
+
+
+--
+-- Data for Name: services; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.services VALUES (1, 'cut', 'Trim or shape hair to desired style/length');
+INSERT INTO public.services VALUES (2, 'color', 'Apply hair coloring (full, highlights, etc.)');
+INSERT INTO public.services VALUES (3, 'perm', 'Create curls or waves with chemicals');
+INSERT INTO public.services VALUES (4, 'style', 'Blowouts, straightening, or updos');
+INSERT INTO public.services VALUES (5, 'trim', 'Remove split ends or maintain style');
+
+
+--
+-- Name: appointments_appointment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.appointments_appointment_id_seq', 25, true);
+
+
+--
+-- Name: customers_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.customers_customer_id_seq', 36, true);
+
+
+--
+-- Name: services_service_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.services_service_id_seq', 5, true);
+
+
+--
+-- Name: appointments appointments_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.appointments
+    ADD CONSTRAINT appointments_pkey PRIMARY KEY (appointment_id);
+
+
+--
+-- Name: customers customers_phone_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.customers
+    ADD CONSTRAINT customers_phone_key UNIQUE (phone);
+
+
+--
+-- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.customers
+    ADD CONSTRAINT customers_pkey PRIMARY KEY (customer_id);
+
+
+--
+-- Name: services services_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.services
+    ADD CONSTRAINT services_pkey PRIMARY KEY (service_id);
+
+
+--
+-- Name: appointments fk_customer_id; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.appointments
+    ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES public.customers(customer_id);
+
+
+--
+-- Name: appointments fk_service_id; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.appointments
+    ADD CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES public.services(service_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
